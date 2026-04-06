f3fef3492d56910d9a566ba15493d8d2b53e68f5
yusuffAFEX <yusufoyedele43@gmail.com>
2026-04-06 03:06:55 +0100

    OFP-1943

--- frappe_agile/frappe_agile/report/sprint_report/sprint_report.js
+++ frappe_agile/frappe_agile/report/sprint_report/sprint_report.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
 frappe.query_reports["Sprint Report"] = {
 	"filters": [
 		{
--- frappe_agile/frappe_agile/report/sprint_report/sprint_report.py
+++ frappe_agile/frappe_agile/report/sprint_report/sprint_report.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
 import frappe
 from frappe.utils import flt, formatdate
 
