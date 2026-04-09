4a70373a695538de3c6d81754b7ae86ce3b74425
samdanikouser <51949767+samdanikouser@users.noreply.github.com>
2026-04-09 13:02:06 +0300

    Delete frappe_agile/frappe_agile/doctype/development_team_member/development_team_member.py

--- frappe_agile/frappe_agile/doctype/development_team_member/development_team_member.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2026, One FM and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
-
-
-class DevelopmentTeamMember(Document):
-	pass
